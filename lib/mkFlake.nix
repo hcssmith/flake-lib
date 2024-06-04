19434f533d61e5f65a53ac90479f51fe56ad0344
Hallam Smith <me@hcssmith.com>
2024-06-04 07:54:56 +0100

    lib changes

--- lib/mkFlake.nix
+++ lib/mkFlake.nix
@@ -1,64 +1,52 @@
 {
   nixpkgs,
   systems,
+}: {
   self,
-}: let
-  defaults = {
-    packages = p: {default = p.hello;};
-    formatter = p: p.alejandra;
-    devShells = p:
-      with p; {
-        default = mkShell {
-          nativeBuildInputs = [nix-prefetch-github];
-          shellHook = "zsh";
-        };
+  overlay ? final: prev: {},
+  overlays ? [self.overlays.default],
+  nixConfig ? {allowUnfree = true;},
+  packages ? p: {default = p.hello;},
+  formatter ? p: p.alejandra,
+  devShells ? p:
+    with p; {
+      default = mkShell {
+        nativeBuildInputs = [nix-prefetch-github];
+        shellHook = "zsh";
       };
-    overlays = [self.overlays.default];
-    overlay = final: prev: {};
-    nixConfig = {
-      allowUnfree = true;
-    };
-  };
+    },
+  ...
+} @ args: let
+  forAllSystems = nixpkgs.lib.genAttrs systems;
+  genOverlay = dir: final: prev: (
+    nixpkgs.lib.genAttrs (builtins.attrNames (builtins.readDir dir)) (
+      name: prev.callPackage /${dir}/${name} {inherit prev;}
+    )
+  );
+  filteredArgs = builtins.removeAttrs args [
+    "overlay"
+    "overlays"
+    "nixConfig"
+    "packages"
+    "formatter"
+  ];
+
+  nixPkgsFor = forAllSystems (
+    system:
+      import nixpkgs {
+        inherit system;
+        overlays = overlays;
+        config = nixConfig;
+      }
+  );
 in
   {
-    overlay ? defaults.overlay,
-    overlays ? defaults.overlays,
-    nixConfig ? defaults.nixConfig,
-    packages ? defaults.packages,
-    formatter ? defaults.formatter,
-    devShells ? defaults.devShells,
-    ...
-  } @ args: let
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-    genOverlay = dir: final: prev: (
-      nixpkgs.lib.genAttrs (builtins.attrNames (builtins.readDir dir)) (
-        name: prev.callPackage /${dir}/${name} {inherit prev;}
-      )
-    );
-    filteredArgs = builtins.removeAttrs args [
-      "overlay"
-      "overlays"
-      "nixConfig"
-      "packages"
-      "formatter"
-    ];
-
-    nixPkgsFor = forAllSystems (
-      system:
-        import nixpkgs {
-          inherit system;
-          overlays = overlays;
-          config = nixConfig;
-        }
-    );
-  in
-    {
-      packages = forAllSystems (system: (packages nixPkgsFor.${system}));
-      formatter = forAllSystems (system: (formatter nixPkgsFor.${system}));
-      devShells = forAllSystems (system: (devShells nixPkgsFor.${system}));
-      overlays.default =
-        if (builtins.isPath overlay)
-        then genOverlay overlay
-        else overlay;
-    }
-    // filteredArgs
+    packages = forAllSystems (system: (packages nixPkgsFor.${system}));
+    formatter = forAllSystems (system: (formatter nixPkgsFor.${system}));
+    devShells = forAllSystems (system: (devShells nixPkgsFor.${system}));
+    overlays.default =
+      if (builtins.isPath overlay)
+      then genOverlay overlay
+      else overlay;
+  }
+  // filteredArgs
