71671e8492345eb598cf196e99c05c335dac1622
Hallam Smith <me@hcssmith.com>
2024-06-04 09:17:49 +0100

    Extra systems

--- flake.nix
+++ flake.nix
@@ -3,13 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs = {
     self,
     nixpkgs,
+    systems,
     ...
   }: {
-    lib = import ./lib {inherit nixpkgs self;};
+    lib = import ./lib {
+      inherit nixpkgs self;
+      systems = import systems;
+    };
   };
 }
