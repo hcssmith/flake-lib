ff446d3fec3ec42c27f6874e4dbf9c34d26a16e7
Hallam Smith <me@hcssmith.com>
2024-06-04 07:56:02 +0100

    Remove unneeded self input

--- lib/default.nix
+++ lib/default.nix
@@ -1,9 +1,8 @@
 {
   nixpkgs,
   systems ? ["x86_64-linux"],
-  self,
   ...
 }: rec {
-  mkFlake = import ./mkFlake.nix {inherit nixpkgs systems self;};
+  mkFlake = import ./mkFlake.nix {inherit nixpkgs systems;};
   mkApp = import ./mkApp.nix {inherit mkFlake;};
 }
